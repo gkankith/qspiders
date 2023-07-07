6b9e5dfb2ffff81a4c3d11fe4acc19721de7a081
rathinreddy1402 <rathinreddy1402@gmail.com>
2023-07-07 11:30:55 +0530

    1st commit

---
+++ MavenPractice/src/test/java/sample/Demo1Test.java
@@ -0,0 +1,32 @@
+package sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	WebDriver driver=null;
+	
+	@Test
+	public void d1Test()
+	{
+		System.out.println("Hi");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		String BROWSER = System.getProperty("browser");
+	/*	String URL = System.getProperty("url");
+		
+		if(BROWSER.equalsIgnoreCase("chrome"))
+		{
+			driver=new ChromeDriver();
+		}else if(BROWSER.equalsIgnoreCase("firefox"))
+		{
+			driver=new FirefoxDriver();
+		}else {
+			driver=new ChromeDriver();
+		}
+		driver.get(URL);*/
+	}
+}
